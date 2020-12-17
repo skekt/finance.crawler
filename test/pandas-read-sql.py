2edb654c7bd49fe1262c9353ddc7ac6fdc2a36e9
maewha <maewha@gmail.com>
2020-12-18 02:21:56 +0900

    wip

--- test/pandas-read-sql.py
+++ test/pandas-read-sql.py
@@ -1,8 +1,8 @@
-def get_search_rankins(query):
-    import pandas as pd
-    import pymysql
-    from datetime import datetime
+import pandas as pd
+import pymysql
+from datetime import datetime
 
+def get_search_rankins(query):
     # DB Connection
     conn = pymysql.connect(host='localhost', port=3306, db='db_finance', user='finance',
                                 password='pwforfinance', charset='utf8')
@@ -29,18 +29,44 @@ def showGraph(rankings):
     import matplotlib.pyplot as plt
 
     print(rankings)
-    print(rankings.company)
-    # plt.title("Search Ranking")
-    # plt.plot(rankings, 'bs--')
-    # plt.xticks(rankings.index)
-    # plt.yticks(rankings.values)
-    # plt.grid(True)
-    # plt.show()
+    # print(rankings.company)
+    # print(f"Distinct entries: {rankings.groupby(['company'])}")
+
+    companies = rankings['company'].unique();
+    print(companies)
+
+    company1 = rankings[rankings.company.eq('메디톡스')]
+    company2 = rankings[rankings.company.eq('대웅')]
+    print(company1)
+    print(company2)
+
+    # print(company1.group_id)
+    # print(company1.ranking)
+
+    # graph_data = {'a': company1.ranking, 'b': company2.ranking}
+    # df = pd.DataFrame(graph_data, index=company1.group_id)
+    # df.plot()
+
+    graph_data = {'a': company1.ranking}
+    df = pd.DataFrame(graph_data)
+
+    # graph_data = {'a': company1.ranking, 'b': company2.ranking}
+    # df = pd.DataFrame(graph_data, index=company1.group_id)
+
+    print(df)
+    df.plot()
+
+    plt.title("Search Ranking")
+    plt.plot(df['a'], 'ro--')
+    # plt.plot(df['b'], 'bo--')
+    # plt.xticks(company1.group_id)
+    # plt.yticks(company1.ranking)
+    plt.grid(True)
+    plt.show()
 
 
 sql = f"select group_id, company, ranking " \
-      f"from search_ranking where created_at >= '2020-12-15' "\
-      f"group by group_id, company, ranking"
+      f"from search_ranking where created_at >= '2020-12-17' "
 
 rankings = get_search_rankins(sql)
 
