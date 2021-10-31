f039457572f19bc681f3ccb1159ebf8cfe7094c9
maewha <maewha@gmail.com>
2021-10-31 10:54:34 +0900

    시간외 단일가 데이터 수집

--- db/migration/1.0.0/V1_0_0_2__create_schema.sql
+++ db/migration/1.0.0/V1_0_0_2__create_schema.sql
@@ -19,3 +19,22 @@ CREATE TABLE company_info
     updated_at DATE,
     INDEX ix_company (company)
 );
+
+# 시간외 단일가
+CREATE TABLE `after_hours` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `market` varchar(10) NOT NULL COMMENT '코스피 / 코스닥',
+  `rank` int(11) NOT NULL COMMENT '순위',
+  `name` varchar(100) NOT NULL DEFAULT '' COMMENT '종목',
+  `symbolCode` varchar(50) NOT NULL DEFAULT '',
+  `code` varchar(50) NOT NULL DEFAULT '',
+  `tradePrice` decimal(10,0) NOT NULL COMMENT '시외 체결가',
+  `changeRate` decimal(10,0) NOT NULL COMMENT '시외 등락률',
+  `accTradeVolume` decimal(10,0) NOT NULL COMMENT '시외 체결량',
+  `regularHoursTradePrice` decimal(10,0) NOT NULL COMMENT '정규장 종가',
+  `regularHoursChange` varchar(10) NOT NULL DEFAULT '' COMMENT '정규장 등락 구분',
+  `regularHoursChangeRate` decimal(10,0) NOT NULL COMMENT '정규장 등락률',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `ix_name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb4;
